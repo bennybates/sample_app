cae0fa345d40aef82cdb6431e751edb3df8d6059
Ben Bates <bates.benjamin@gmail.com>
2012-05-14 22:23:54 -0400

    Setting up Users (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 SampleApp::Application.routes.draw do
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
   get "users/new"
   
   root to: 'static_pages#home'
