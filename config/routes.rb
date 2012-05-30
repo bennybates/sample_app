e2db00f2393a65f2efca15370783abee7dc67ae6
Ben Bates <bates.benjamin@gmail.com>
2012-05-29 22:15:44 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-  match '/signup', to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,7 @@ describe User do
 
   before do
   	@user = User.new(name: "Example User", email: "user@example.com",
-  					 password: "foobar", password_confirmation: "foobar")	
+  					         password: "foobar", password_confirmation: "foobar")	
   end	
 
   subject { @user }
@@ -26,10 +26,16 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end 
+  
   describe "when name is not present" do
   	before { @user.name = " " }
   	it { should_not be_valid }
